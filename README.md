2a218ade09fc3528ba4b6bf171878b2920683260
qq1012803704 <1012803704@qq.com>
2016-02-23 00:07:16 +0800

    create
    create

---
+++ README.md
@@ -0,0 +1,40 @@
+# README #
+
+The Unity UI system is open and available for use in your games and applications. This document details how to get it up and running on your computer and start modifying the code!
+
+### What license is the UI system shipped under? ###
+The UI system is released under an MIT/X11 license; see the LICENSE file.
+
+This means that you pretty much can customize and embed it in any software under any license without any other constraints than preserving the copyright and license information while adding your own copyright and license information.
+
+You can keep the source to yourself or share your customized version under the same MIT license or a compatible license.
+
+If you want to contribute patches back, please keep it under the unmodified MIT license so it can be integrated in future versions and shared under the same license.
+
+### How do I get started? ###
+* Clone this repository onto a location on your computer.
+* Configure your IDE for the Unity coding standard, look in the .editorconfig file for more information
+* Open the project in Visual Studio or MonoDevelop
+    * If you are using MonoDevelop
+        * Ensure you enable XBuild (Preferences -> Projects -> Build ->"Compile projects using MSBuild/XBuild")
+        * You may need to restart MonoDevelop
+    * Build the solution
+
+* A folder will be created in the root directory called "Output", the generated dll's will output here in the correct folder structure
+    * If you wish to use these dll's
+    * Locate your Unity install location
+    * Windows: Copy the contents of Output folder to: `Data\UnityExtensions\Unity\GUISystem\{UNITY_VERSION}`
+    * OSX: Copy the contents of Output folder to: `Unity.app/Contents/UnityExtensions/Unity/GUISystem/{UNITY_VERSION}`
+
+* If you want the dll's to copy automatically on build
+    * For each visual studio project file
+        * Open the file in a text editor
+        * Locate the section: <Target Name="AfterBuild">
+        * Follow the instructions in the comments
+
+### Unity is at version X but the repo is only at version Y why isn't it updated already? ###
+
+Unity's release schedule doesn't always happen at convenient times for us to drop everything and update our public repo. We will try to always have the repo updated within a week of a public release but sometimes things happen and more time is needed. Please note that we will NOT be updating the repo for patch releases due to the time it takes to update the repo at this time.
+
+### Will you be taking pull requests? ###
+At this stage the UI system is undergoing further development internally with many design decisions still being debated and implemented. We will be accepting pull requests when the team has time to process them. Due to the current set up where development actually happens on a internal repo, changes are pushed in a batch when public releases are done. Whether a pull request is accepted or not will be declined locally with a comment of Accepted. This makes for less merge conflicted during the batch pushes as well as ensuring a fix will make it out in the official unity release for everyone.
---
+++ UnityEditor.UI/UI/MenuOptions.cs
@@ -0,0 +1,283 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UnityEditor.UI
+{
+    /// <summary>
+    /// This script adds the UI menu options to the Unity Editor.
+    /// </summary>
+
+    static internal class MenuOptions
+    {
+        private const string kUILayerName = "UI";
+
+        private const string kStandardSpritePath       = "UI/Skin/UISprite.psd";
+        private const string kBackgroundSpritePath     = "UI/Skin/Background.psd";
+        private const string kInputFieldBackgroundPath = "UI/Skin/InputFieldBackground.psd";
+        private const string kKnobPath                 = "UI/Skin/Knob.psd";
+        private const string kCheckmarkPath            = "UI/Skin/Checkmark.psd";
+        private const string kDropdownArrowPath        = "UI/Skin/DropdownArrow.psd";
+        private const string kMaskPath                 = "UI/Skin/UIMask.psd";
+
+        static private DefaultControls.Resources s_StandardResources;
+
+        static private DefaultControls.Resources GetStandardResources()
+        {
+            if (s_StandardResources.standard == null)
+            {
+                s_StandardResources.standard = AssetDatabase.GetBuiltinExtraResource<Sprite>(kStandardSpritePath);
+                s_StandardResources.background = AssetDatabase.GetBuiltinExtraResource<Sprite>(kBackgroundSpritePath);
+                s_StandardResources.inputField = AssetDatabase.GetBuiltinExtraResource<Sprite>(kInputFieldBackgroundPath);
+                s_StandardResources.knob = AssetDatabase.GetBuiltinExtraResource<Sprite>(kKnobPath);
+                s_StandardResources.checkmark = AssetDatabase.GetBuiltinExtraResource<Sprite>(kCheckmarkPath);
+                s_StandardResources.dropdown = AssetDatabase.GetBuiltinExtraResource<Sprite>(kDropdownArrowPath);
+                s_StandardResources.mask = AssetDatabase.GetBuiltinExtraResource<Sprite>(kMaskPath);
+            }
+            return s_StandardResources;
+        }
+
+        private static void SetPositionVisibleinSceneView(RectTransform canvasRTransform, RectTransform itemTransform)
+        {
+            // Find the best scene view
+            SceneView sceneView = SceneView.lastActiveSceneView;
+            if (sceneView == null && SceneView.sceneViews.Count > 0)
+                sceneView = SceneView.sceneViews[0] as SceneView;
+
+            // Couldn't find a SceneView. Don't set position.
+            if (sceneView == null || sceneView.camera == null)
+                return;
+
+            // Create world space Plane from canvas position.
+            Vector2 localPlanePosition;
+            Camera camera = sceneView.camera;
+            Vector3 position = Vector3.zero;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRTransform, new Vector2(camera.pixelWidth / 2, camera.pixelHeight / 2), camera, out localPlanePosition))
+            {
+                // Adjust for canvas pivot
+                localPlanePosition.x = localPlanePosition.x + canvasRTransform.sizeDelta.x * canvasRTransform.pivot.x;
+                localPlanePosition.y = localPlanePosition.y + canvasRTransform.sizeDelta.y * canvasRTransform.pivot.y;
+
+                localPlanePosition.x = Mathf.Clamp(localPlanePosition.x, 0, canvasRTransform.sizeDelta.x);
+                localPlanePosition.y = Mathf.Clamp(localPlanePosition.y, 0, canvasRTransform.sizeDelta.y);
+
+                // Adjust for anchoring
+                position.x = localPlanePosition.x - canvasRTransform.sizeDelta.x * itemTransform.anchorMin.x;
+                position.y = localPlanePosition.y - canvasRTransform.sizeDelta.y * itemTransform.anchorMin.y;
+
+                Vector3 minLocalPosition;
+                minLocalPosition.x = canvasRTransform.sizeDelta.x * (0 - canvasRTransform.pivot.x) + itemTransform.sizeDelta.x * itemTransform.pivot.x;
+                minLocalPosition.y = canvasRTransform.sizeDelta.y * (0 - canvasRTransform.pivot.y) + itemTransform.sizeDelta.y * itemTransform.pivot.y;
+
+                Vector3 maxLocalPosition;
+                maxLocalPosition.x = canvasRTransform.sizeDelta.x * (1 - canvasRTransform.pivot.x) - itemTransform.sizeDelta.x * itemTransform.pivot.x;
+                maxLocalPosition.y = canvasRTransform.sizeDelta.y * (1 - canvasRTransform.pivot.y) - itemTransform.sizeDelta.y * itemTransform.pivot.y;
+
+                position.x = Mathf.Clamp(position.x, minLocalPosition.x, maxLocalPosition.x);
+                position.y = Mathf.Clamp(position.y, minLocalPosition.y, maxLocalPosition.y);
+            }
+
+            itemTransform.anchoredPosition = position;
+            itemTransform.localRotation = Quaternion.identity;
+            itemTransform.localScale = Vector3.one;
+        }
+
+        private static void PlaceUIElementRoot(GameObject element, MenuCommand menuCommand)
+        {
+            GameObject parent = menuCommand.context as GameObject;
+            if (parent == null || parent.GetComponentInParent<Canvas>() == null)
+            {
+                parent = GetOrCreateCanvasGameObject();
+            }
+
+            string uniqueName = GameObjectUtility.GetUniqueNameForSibling(parent.transform, element.name);
+            element.name = uniqueName;
+            Undo.RegisterCreatedObjectUndo(element, "Create " + element.name);
+            Undo.SetTransformParent(element.transform, parent.transform, "Parent " + element.name);
+            GameObjectUtility.SetParentAndAlign(element, parent);
+            if (parent != menuCommand.context) // not a context click, so center in sceneview
+                SetPositionVisibleinSceneView(parent.GetComponent<RectTransform>(), element.GetComponent<RectTransform>());
+
+            Selection.activeGameObject = element;
+        }
+
+        // Graphic elements
+
+        [MenuItem("GameObject/UI/Text", false, 2000)]
+        static public void AddText(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreateText(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+        }
+
+        [MenuItem("GameObject/UI/Image", false, 2001)]
+        static public void AddImage(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreateImage(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+        }
+
+        [MenuItem("GameObject/UI/Raw Image", false, 2002)]
+        static public void AddRawImage(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreateRawImage(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+        }
+
+        // Controls
+
+        // Button and toggle are controls you just click on.
+
+        [MenuItem("GameObject/UI/Button", false, 2030)]
+        static public void AddButton(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreateButton(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+        }
+
+        [MenuItem("GameObject/UI/Toggle", false, 2031)]
+        static public void AddToggle(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreateToggle(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+        }
+
+        // Slider and Scrollbar modify a number
+
+        [MenuItem("GameObject/UI/Slider", false, 2033)]
+        static public void AddSlider(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreateSlider(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+        }
+
+        [MenuItem("GameObject/UI/Scrollbar", false, 2034)]
+        static public void AddScrollbar(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreateScrollbar(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+        }
+
+        // More advanced controls below
+
+        [MenuItem("GameObject/UI/Dropdown", false, 2035)]
+        static public void AddDropdown(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreateDropdown(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+        }
+
+        [MenuItem("GameObject/UI/Input Field", false, 2036)]
+        public static void AddInputField(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreateInputField(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+        }
+
+        // Containers
+
+        [MenuItem("GameObject/UI/Canvas", false, 2060)]
+        static public void AddCanvas(MenuCommand menuCommand)
+        {
+            var go = CreateNewUI();
+            GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+            if (go.transform.parent as RectTransform)
+            {
+                RectTransform rect = go.transform as RectTransform;
+                rect.anchorMin = Vector2.zero;
+                rect.anchorMax = Vector2.one;
+                rect.anchoredPosition = Vector2.zero;
+                rect.sizeDelta = Vector2.zero;
+            }
+            Selection.activeGameObject = go;
+        }
+
+        [MenuItem("GameObject/UI/Panel", false, 2061)]
+        static public void AddPanel(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreatePanel(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+
+            // Panel is special, we need to ensure there's no padding after repositioning.
+            RectTransform rect = go.GetComponent<RectTransform>();
+            rect.anchoredPosition = Vector2.zero;
+            rect.sizeDelta = Vector2.zero;
+        }
+
+        [MenuItem("GameObject/UI/Scroll View", false, 2062)]
+        static public void AddScrollView(MenuCommand menuCommand)
+        {
+            GameObject go = DefaultControls.CreateScrollView(GetStandardResources());
+            PlaceUIElementRoot(go, menuCommand);
+        }
+
+        // Helper methods
+
+        static public GameObject CreateNewUI()
+        {
+            // Root for the UI
+            var root = new GameObject("Canvas");
+            root.layer = LayerMask.NameToLayer(kUILayerName);
+            Canvas canvas = root.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            root.AddComponent<CanvasScaler>();
+            root.AddComponent<GraphicRaycaster>();
+            Undo.RegisterCreatedObjectUndo(root, "Create " + root.name);
+
+            // if there is no event system add one...
+            CreateEventSystem(false);
+            return root;
+        }
+
+        [MenuItem("GameObject/UI/Event System", false, 2100)]
+        public static void CreateEventSystem(MenuCommand menuCommand)
+        {
+            GameObject parent = menuCommand.context as GameObject;
+            CreateEventSystem(true, parent);
+        }
+
+        private static void CreateEventSystem(bool select)
+        {
+            CreateEventSystem(select, null);
+        }
+
+        private static void CreateEventSystem(bool select, GameObject parent)
+        {
+            var esys = Object.FindObjectOfType<EventSystem>();
+            if (esys == null)
+            {
+                var eventSystem = new GameObject("EventSystem");
+                GameObjectUtility.SetParentAndAlign(eventSystem, parent);
+                esys = eventSystem.AddComponent<EventSystem>();
+                eventSystem.AddComponent<StandaloneInputModule>();
+                eventSystem.AddComponent<TouchInputModule>();
+
+                Undo.RegisterCreatedObjectUndo(eventSystem, "Create " + eventSystem.name);
+            }
+
+            if (select && esys != null)
+            {
+                Selection.activeGameObject = esys.gameObject;
+            }
+        }
+
+        // Helper function that returns a Canvas GameObject; preferably a parent of the selection, or other existing Canvas.
+        static public GameObject GetOrCreateCanvasGameObject()
+        {
+            GameObject selectedGo = Selection.activeGameObject;
+
+            // Try to find a gameobject that is the selected GO or one if its parents.
+            Canvas canvas = (selectedGo != null) ? selectedGo.GetComponentInParent<Canvas>() : null;
+            if (canvas != null && canvas.gameObject.activeInHierarchy)
+                return canvas.gameObject;
+
+            // No canvas in selection or its parents? Then use just any canvas..
+            canvas = Object.FindObjectOfType(typeof(Canvas)) as Canvas;
+            if (canvas != null && canvas.gameObject.activeInHierarchy)
+                return canvas.gameObject;
+
+            // No canvas in the scene at all? Then create a new one.
+            return MenuOptions.CreateNewUI();
+        }
+    }
+}
